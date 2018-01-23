a15d42993b5f24700e51d9cebdbb44d9c617b75b
Odai-kakhi <odaikakhi@gmail.com>
2018-01-23 23:19:50 +0100

    Done !

--- js/console-todoList.js/index.js
+++ js/console-todoList.js/index.js
@@ -5,24 +5,31 @@ var input = prompt('What would you like to Do')
 
 function toDo(input) {
     var list = []
-while (input.indexOf('quite') == -1 ) {
-     input = prompt('What would you like to Do')
-     switch (input) {
-        case 'add':
-            var item = prompt('Add to list')
-            list.push(item)
-            console.log(list)
-            break;
+    while (input.indexOf('quite') == -1) {
+        input = prompt('What would you like to Do')
+        switch (input) {
+            case 'add':
+                var item = prompt('Add to list')
+                list.push(item)
+                console.log(item + ' adedd to the list')
+                break;
 
-        case 'list':
-            console.log(list)
-            break;
+            case 'list':
+                list.forEach((item) => {
+                    console.log(list.indexOf(item) + ' : ' + item)
+                })
+                break;
 
-        default:
-            break;
+            case 'delete':
+               console.log('Deleted Item is : ' + list.pop())
+
+                break;
+
+            default:
+                break;
+        }
     }
-}
-   
+
 }
 
 
